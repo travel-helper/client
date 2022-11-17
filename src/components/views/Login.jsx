c92237db748d0a707d7851332aafaa7d0a63e871
Lcheck <znfhdl789@naver.com>
2022-11-17 15:34:29 +0900

    Fix 로그인 데이터 전달 형식 변경 및 주소 변경

--- src/components/views/Login.jsx
+++ src/components/views/Login.jsx
@@ -5,58 +5,74 @@ import Form from "react-bootstrap/Form";
 import { Link } from "react-router-dom";
 
 const LoginBtnClick = (email, password) => {
-    const dataForm = new FormData();
-    dataForm.append("id",email);
-    dataForm.append("password",password)
-    axios.post("/api/auth/login",dataForm).then((res)=>{
-        if(!!res){
-            alert("로그인 성공")
-        }
-    }).catch((res)=>{
-        alert(res+"에러 발생")
-    })    
-}
+  // const dataForm = new FormData();
+  // dataForm.append("id",email);
+  // dataForm.append("password",password)
+
+  const form = { email, password };
+  axios
+    .post("/user/login", form)
+    .then((res) => {
+      if (!!res) {
+        alert("로그인 성공");
+      }
+    })
+    .catch((res) => {
+      alert(res + "에러 발생");
+    });
+};
 const Login = () => {
-    return (
-            <Container className="login_page">
-                <Form className="w-50">
-                    <Form.Group className="mb-1" controlId="formBasicEmail">
-                        <Form.Control id="email" type="email" placeholder="이메일" className="bg-secondary bg-opacity-10" />
-                    </Form.Group>
+  return (
+    <Container className="login_page">
+      <Form className="w-50">
+        <Form.Group className="mb-1" controlId="formBasicEmail">
+          <Form.Control
+            id="email"
+            type="email"
+            placeholder="이메일"
+            className="bg-secondary bg-opacity-10"
+          />
+        </Form.Group>
 
-                    <Form.Group className="mb-3" controlId="formBasicPassword">
-                        <Form.Control id="password" type="password" placeholder="비밀번호" className="bg-secondary bg-opacity-10" />
-                    </Form.Group>
-                    <button className="login_join_btn" onClick={()=>{
-                        const email = document.getElementById("email").value
-                        const password = document.getElementById("password").value
-                        LoginBtnClick(email, password);
-                    }}>
-                        로그인
-                    </button>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Control
+            id="password"
+            type="password"
+            placeholder="비밀번호"
+            className="bg-secondary bg-opacity-10"
+          />
+        </Form.Group>
+        <button
+          className="login_join_btn"
+          onClick={() => {
+            const email = document.getElementById("email").value;
+            const password = document.getElementById("password").value;
+            LoginBtnClick(email, password);
+          }}
+        >
+          로그인
+        </button>
 
-                    <button className="pw_update_btn">
-                        비밀번호 재설정
-                    </button>
-                    
-                    <button className="btn_design">
-                        <Link to="/join" id="link">회원가입</Link>
-                    </button>
-                    
-                    <br/>
-                    <br/>
-                    <hr/>
-                    <p className="sns_text">sns로 간편 로그인 및 회원가입</p>
+        <button className="pw_update_btn">비밀번호 재설정</button>
 
-                    <div className="sns_login_btn">
-                        <button className="kakao_login_join_btn"></button>
-                        <button className="naver_login_join_btn"></button>
-                        <button className="facebook_login_join_btn"></button>
-                    </div>
-                    
-                </Form>
-            </Container>
-    
+        <button className="btn_design">
+          <Link to="/join" id="link">
+            회원가입
+          </Link>
+        </button>
+
+        <br />
+        <br />
+        <hr />
+        <p className="sns_text">sns로 간편 로그인 및 회원가입</p>
+
+        <div className="sns_login_btn">
+          <button className="kakao_login_join_btn"></button>
+          <button className="naver_login_join_btn"></button>
+          <button className="facebook_login_join_btn"></button>
+        </div>
+      </Form>
+    </Container>
   );
 };
 
