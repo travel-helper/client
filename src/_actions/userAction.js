6861b8cc64e5a7da5c32f4cb4c681c03b8bf9179
Lcheck <znfhdl789@naver.com>
2022-11-29 16:07:23 +0900

    Fix jwt

--- src/_actions/userAction.js
+++ src/_actions/userAction.js
@@ -5,7 +5,7 @@ import {
   requestSignUp,
   requestlogout,
 } from "../api/api";
-
+import jwt from "jsonwebtoken";
 export const login = createAsyncThunk(
   "user/login",
   async (data, { rejectWithValue }) => {
@@ -14,6 +14,7 @@ export const login = createAsyncThunk(
       // console.log(token);
       localStorage.setItem("jwtToken", token);
       // console.log("localstorage set jwt");
+
       return token;
     } catch (error) {
       return rejectWithValue(error.response.data);
@@ -24,7 +25,8 @@ export const login = createAsyncThunk(
 export const loadMyInfo = createAsyncThunk("user/loadMyInfo", async () => {
   //로그인 유지를 위한 유저 정보 요청 함수
   const result = await fetchUser();
-  return result;
+  
+  return res
 });
 
 export const logout = createAsyncThunk("user/logout", async () => {
